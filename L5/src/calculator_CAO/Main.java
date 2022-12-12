9cc02a0d7b8981b46ca05d890682d1ec16522793
Danyil Tymchuk <71641218+DanyilT@users.noreply.github.com>
2022-12-12 11:50:40 +0000

    Add files via upload

---
+++ L5/src/calculator_CAO/Main.java
@@ -0,0 +1,47 @@
+package calculator_CAO;
+/*
+Created by: Dany
+Created on 02/12/2022 : 13:47
+This is program to CAO calculator
+*/
+
+import java.util.Scanner;
+
+import static calculator_CAO.Calculator.*;
+
+public class Main extends logUp{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+// WELCOME
+        System.out.println("\t\t WELCOME TO CAO CALCULATOR");
+        System.out.print("------------------------------------\n");
+
+// LOG UP
+        LogUp();
+
+// LOG IN
+        LogIn();
+
+// INSTRUCTIONS
+        System.out.println("\n\t\tINSTRUCTIONS");
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+
+// EXIT
+        System.out.println("Enter 'EXIT' if you want finish it");
+        String exit = scanner.nextLine();
+        if (exit.toLowerCase().contains("exit")) {
+            System.out.println("--------------");
+            System.out.println(" EXIT PROGRAM");
+            System.out.println("--------------");
+            System.exit(0);
+        }
+// CALCULATOR
+        printInfo();
+
+        System.out.println("--------------");
+        System.out.println(" EXIT PROGRAM");
+        System.out.println("--------------");
+    }
+}
